47945af17254c3bb47a8fc8bcbf1275e80e43522
psyrun <psyrun@localhost.localdomain>
2020-03-11 17:54:08 +0530

    updated

--- Escape-sequence.py
+++ Escape-sequence.py
@@ -1,13 +1,46 @@
-#Escape sequence
+#Escape sequence 
 
-print("this is \\\\ double backslash  \n")
-print("this is /\\/\\/\\/\\ mountain \n")
-print("He is \t awesome")
-print("\\ \" \\n \\t \\ \'")
+#New line character
+print("Hello earth ! \nHello world !\n") 
+
+#Tab space
+print("Hello earth ! \tHello world !\n")
+
+#Back space
+print("Hello earth ! \bHello world ! \n\n")
+
+
+#Printing Escape sequence with \\ double slash 
+
+print("printing double \\\\ ")
+print("printing mountain /\\/\\/\\/\\")
+print("printing \\t tab escape character")
+print("\\ \" \\n \\t \\ \' \n\n")
+
+
+
+print(r"printing double \\   \n")
+print(r"printing mountain /\/\/\/\ \n")
+print(r"printing \t tab escape character")
+print(r"\ \" \n \t \ \'")
 
 #output
 
-# this is \\ double backslash  
-# this is /\/\/\/\ mountain 
-# He is    awesome
-# \ " \n \t \ '
+# Hello earth ! 
+# Hello world !
+
+# Hello earth !   Hello world !
+
+# Hello earth !Hello world ! 
+
+
+# printing double \\ 
+# printing mountain /\/\/\/\
+# printing \t tab escape character
+# \ " \n \t \ ' 
+
+
+# printing double \\   \n
+# printing mountain /\/\/\/\ \n
+# printing \t tab escape character
+# \ \" \n \t \ \'
