958a02c2df1cb66e520585e2a4f84d9458530ccc
psyrun <psyrun@localhost.localdomain>
2020-03-11 21:09:23 +0530

    update

---
+++ Unary_operator.py
@@ -0,0 +1,21 @@
+#unary operation
+
+a = 1
+
+# "+" Plus operator
+a =+a
+print(a)
+
+# "~" Bitwise inversion 
+a =~a
+print(a)
+
+# "-" Subtraction operator
+a -=a
+print(a)
+
+# output
+
+# 1
+# -2
+# 0
