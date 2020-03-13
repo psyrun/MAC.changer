96643528be67f4cca2e6bdafca167a8519470dc2
psyrun <psyrun@localhost.localdomain>
2020-03-13 13:10:43 +0530

    update

--- Control_statements.py
+++ Control_statements.py
@@ -2,21 +2,25 @@
 
 #If-elif-else
 
-a = int(input())
-if(a>0):
-    print("Unit greater than zero")
-elif(a<0):
-    print("Unit smaller than zero")
-else:
-    print("Unit equal to zero")
+# a = int(input())
+# if(a>0):
+#     print("Unit greater than zero")
+# elif(a<0):
+#     print("Unit smaller than zero")
+# else:
+#     print("Unit equal to zero")
 
 
 #While
 
-b=int(input())
-while(True):
-    print("While Loop")
-    ++b
+# count=int(input())
+# while(count>0):
+#     print("While Loop",count)
+#     count = count +1
+# print("exit")
+
+#for
+count 
 
 
 #Do while
