cd0409d48d6c1ef8db688064f29b527c63d6dfe2
psyrun <psyrun@localhost.localdomain>
2020-03-13 13:00:56 +0530

    updated/bin/python3.8

---
+++ Control_statements.py
@@ -0,0 +1,37 @@
+# To control the flow of an equation.
+
+#If-elif-else
+
+a = int(input())
+if(a>0):
+    print("Unit greater than zero")
+elif(a<0):
+    print("Unit smaller than zero")
+else:
+    print("Unit equal to zero")
+
+
+#While
+
+b=int(input())
+while(True):
+    print("While Loop")
+    ++b
+
+
+#Do while
+
+
+#Nested Control
+
+
+#Switch Statement
+
+
+#Continue Statement
+
+
+#Comma operator
+
+
+#Goto Statement
